bbcac15ac83794ebabe92784309d1e66064e30fd
Mihai S. <smeu.s.miha@gmail.com>
2026-03-08 23:01:04 +0200

    refactor: Extrage valoarea bonus intr-o constanta

--- Calculator.java
+++ Calculator.java
@@ -1,9 +1,9 @@
 public class Calcualtor {
+	//BONUS devine o constanta a clasei
+	private static final int BONUS = 2;
 	//Calculeaza suma a doua numere
 	public int aduna(int a, int b) {
-		// Temporar, adunam si un bonus hardcodat
-		int bonus = 2;
-		return a + b + bonus;
+		return a + b + BONUS;
 	}
 	
 	public static void main (String[] args) {
